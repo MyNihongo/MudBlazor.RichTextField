4692362d82b22740418c23dcfda0da0f9f97a4e4
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-03-20 22:05:17 +0200

    empty if <br>

--- src/MudBlazor.RichTextField/Utils/Extensions/StringEx.cs
+++ src/MudBlazor.RichTextField/Utils/Extensions/StringEx.cs
@@ -7,7 +7,7 @@ internal static class StringEx
 	/// </summary>
 	public static string FromInnerHtml(this string? @this)
 	{
-		if (string.IsNullOrEmpty(@this))
+		if (string.IsNullOrEmpty(@this) || @this == "<br>")
 			return string.Empty;
 
 		return @this;
