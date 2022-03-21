8a94c7c0fa5ec11342e89b99e80acf57f43e72d7
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-03-21 23:26:23 +0200

    set RTL

--- samples/MudBlazor.RichTextField.Sample.Core/Services/Interfaces/IThemeService.cs
+++ samples/MudBlazor.RichTextField.Sample.Core/Services/Interfaces/IThemeService.cs
@@ -4,7 +4,11 @@ public interface IThemeService
 {
 	IObservable<bool> IsDarkTheme { get; }
 
+	IObservable<bool> IsRightToLeft { get; }
+
 	Task InitAsync();
 
 	Task ToggleThemeAsync();
+
+	Task ToggleRightToLeftAsync();
 }
--- samples/MudBlazor.RichTextField.Sample.Core/Services/ThemeService.cs
+++ samples/MudBlazor.RichTextField.Sample.Core/Services/ThemeService.cs
@@ -4,12 +4,12 @@ namespace MudBlazor.RichTextField;
 
 internal sealed class ThemeService : IThemeService
 {
-	private const string IsDarkKey = "HnT";
+	private const string IsDarkKey = "HnT", IsRtlKey = "NNb";
 
-	private readonly ReplaySubject<bool> _isDarkThemeSubject = new(1);
+	private readonly ReplaySubject<bool> _isDarkThemeSubject = new(1), _isRtlSubject = new(1);
 	private readonly ILocalStorageService _localStorageService;
 
-	private bool _isDark;
+	private bool _isDark, _isRtl;
 
 	public ThemeService(ILocalStorageService localStorageService)
 	{
@@ -18,12 +18,18 @@ internal sealed class ThemeService : IThemeService
 
 	public IObservable<bool> IsDarkTheme => _isDarkThemeSubject;
 
+	public IObservable<bool> IsRightToLeft => _isRtlSubject;
+
 	public async Task InitAsync()
 	{
 		_isDark = await _localStorageService.GetItemAsync<bool>(IsDarkKey)
 			.ConfigureAwait(false);
 
+		_isRtl = await _localStorageService.GetItemAsync<bool>(IsRtlKey)
+			.ConfigureAwait(false);
+
 		PublishTheme(_isDark);
+		PublishRtl(_isRtl);
 	}
 
 	public async Task ToggleThemeAsync()
@@ -36,8 +42,23 @@ internal sealed class ThemeService : IThemeService
 		PublishTheme(_isDark);
 	}
 
+	public async Task ToggleRightToLeftAsync()
+	{
+		_isRtl = !_isRtl;
+
+		await _localStorageService.SetItemAsync(IsRtlKey, _isRtl)
+			.ConfigureAwait(false);
+
+		PublishRtl(_isRtl);
+	}
+
 	private void PublishTheme(bool isDark)
 	{
 		_isDarkThemeSubject.OnNext(isDark);
+	}
+
+	private void PublishRtl(bool isRtl)
+	{
+		_isRtlSubject.OnNext(isRtl);
 	}
 }
