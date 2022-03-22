b2b8d7d7b5d207c1f1ce97ae527eea36341786c9
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-03-22 22:23:52 +0200

    tests for ToValue

--- src/MudBlazor.RichTextField/Utils/Extensions/StringEx.cs
+++ src/MudBlazor.RichTextField/Utils/Extensions/StringEx.cs
@@ -1,36 +1,82 @@
-﻿namespace MudBlazor;
+﻿using System.Text;
+using Microsoft.Extensions.ObjectPool;
+
+namespace MudBlazor;
 
 internal static class StringEx
 {
 	private const string Heading = "<div>", Trailing = "</div>", Break = "<br>";
 
+	private static readonly ObjectPool<StringBuilder> StringBuilderPool = new DefaultObjectPoolProvider()
+		.CreateStringBuilderPool();
+
 	/// <summary>
-	/// Converts the innerHTML to a normal RichText string
+	/// Converts the innerHTML to a RichText string
 	/// </summary>
 	public static string ToValue(this string? @this)
 	{
 		if (string.IsNullOrEmpty(@this) || @this == Break)
 			return string.Empty;
 
-		return @this
-			.Replace(Heading, string.Empty)
-			.Replace(Break, string.Empty)
-			.RemoveTrailingDiv()
-			.Replace(Trailing, Environment.NewLine);
+		var builder = StringBuilderPool.Get();
+		var lineStart = @this.StartsWith(Heading) ? Heading.Length : 0;
+		var end = @this.EndsWith(Trailing) ? @this.Length - Trailing.Length : @this.Length;
+
+		for (var i = lineStart; i < end; i++)
+		{
+			if (StartsWith(@this, Heading, i))
+			{
+				if (lineStart < i)
+					builder.AppendLine(@this[lineStart..i]);
+
+				lineStart = i + Heading.Length;
+				i = lineStart - 1;
+			}
+			else if (StartsWith(@this, Trailing, i))
+			{
+				builder.AppendLine(@this[lineStart..i]);
+				lineStart = i + Trailing.Length;
+				i = lineStart - 1;
+			}
+			else if (StartsWith(@this, Break, i))
+			{
+				lineStart = i + Break.Length;
+				i = lineStart - 1;
+			}
+		}
+
+		if (lineStart < end)
+			builder.Append(@this[lineStart..end]);
+
+		return builder.ToString();
 	}
 
+	/// <summary>
+	/// Converts the RichText string to an innerHTML
+	/// </summary>
 	public static string ToInnerHtml(this string? @this)
 	{
 		if (string.IsNullOrEmpty(@this))
 			return Break;
 
+		const string newLine = Trailing + Heading;
+
+		@this = @this
+			.Replace(Environment.NewLine, newLine)
+			.Replace("\n", newLine);
+
 		return Heading + @this + Trailing;
 	}
 
-	private static string RemoveTrailingDiv(this string @this)
+	private static bool StartsWith(in string @this, in string value, in int start)
 	{
-		return @this.EndsWith(Trailing)
-			? @this[..^Trailing.Length]
-			: @this;
+		if (start + value.Length > @this.Length)
+			return false;
+
+		for (var i = 0; i < value.Length; i++)
+			if (value[i] != @this[start + i])
+				return false;
+
+		return true;
 	}
 }
---
+++ tests/MudBlazor.RichTextField.Tests/Utils/StringExTests/ToValueShould.cs
@@ -0,0 +1,107 @@
+﻿namespace MudBlazor.RichTextField.Tests.Utils.StringExTests;
+
+public sealed class ToValueShould
+{
+	[Fact]
+	public void NotChangeIfNoTags()
+	{
+		const string input = nameof(input);
+
+		var result = input.ToValue();
+
+		result
+			.Should()
+			.Be(input);
+	}
+
+	[Fact]
+	public void ConvertValueAndDiv()
+	{
+		const string input = "input<div>new line</div>";
+		var expected = "input" + Environment.NewLine + "new line";
+
+		var result = input.ToValue();
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void ConvertTwoDivs()
+	{
+		const string input = "<div>input</div><div>new line</div>";
+		var expected = "input" + Environment.NewLine + "new line";
+
+		var result = input.ToValue();
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void ConvertDivAndValue()
+	{
+		const string input = "<div>input</div>new line";
+		var expected = "input" + Environment.NewLine + "new line";
+
+		var result = input.ToValue();
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void ConvertThreeDivs()
+	{
+		const string input = "<div>line1</div><div>line2</div><div>line3</div>";
+		var expected = "line1" + Environment.NewLine + "line2" + Environment.NewLine + "line3";
+
+		var result = input.ToValue();
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void KeepItalic()
+	{
+		const string input = "some <i>italic</i> value<div>new <i>line</i></div>";
+		var expected = "some <i>italic</i> value" + Environment.NewLine + "new <i>line</i>";
+
+		var result = input.ToValue();
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void KeepBold()
+	{
+		const string input = "some <b>italic</b> value<div>new <b>line</b></div>";
+		var expected = "some <b>italic</b> value" + Environment.NewLine + "new <b>line</b>";
+
+		var result = input.ToValue();
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void ConvertEmptyLine()
+	{
+		const string input = "<div>line1</div><div><br></div><div>line3</div>";
+		var expected = "line1" + Environment.NewLine + Environment.NewLine + "line3";
+
+		var result = input.ToValue();
+
+		result
+			.Should()
+			.Be(expected);
+	}
+}
