8dab19c67b834c1f1fbd23fd3455fe5eb65317ac
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-03-25 23:10:21 +0200

    add toggle buttons

--- src/MudBlazor.RichTextField/Components/MudRichTextToolbar.razor.cs
+++ src/MudBlazor.RichTextField/Components/MudRichTextToolbar.razor.cs
@@ -4,4 +4,8 @@ public sealed partial class MudRichTextToolbar : ComponentBase
 {
 	[Parameter]
 	public MudRichTextField TextField { get; set; } = default!;
+
+	private bool IsBoldActive { get; set; }
+
+	private bool IsItalicActive { get; set; }
 }
