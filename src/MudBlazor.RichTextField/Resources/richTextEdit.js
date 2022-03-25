29f677c0fcb7573320497f928f984b336763eb66
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-03-26 00:06:13 +0200

    add a function

--- src/MudBlazor.RichTextField/Resources/richTextEdit.js
+++ src/MudBlazor.RichTextField/Resources/richTextEdit.js
@@ -22,12 +22,13 @@ MudBlazorRichTextEdit.init = (elementId, dotNetInvokable) => {
 		await dotNetInvokable.invokeMethodAsync("SetValue", target.innerHTML);
 	};
 
-	const onClick = function(e) {
-		console.log(window.getSelection(), e.target);
+	const onClick = function (e) {
+		console.log("onclick", e.target);
+		setCurrentSelection(elementId, e.target);
 	};
 
-	const onKeyUp = function() {
-		console.log("keyup");
+	const onKeyUp = function(e) {
+		console.log("keyup", e);
 	};
 
 	const observer = new MutationObserver(onInnerHtmlChanged);
@@ -84,4 +85,28 @@ function removeMudShrink(element) {
 	}
 
 	element.classList.remove(mudShrink);
+}
+
+// for KeyUp
+function getCaretIndex(element) {
+	let position = 0;
+	const isSupported = typeof window.getSelection !== "undefined";
+	if (isSupported) {
+		const selection = window.getSelection();
+		// Check if there is a selection (i.e. cursor in place)
+		if (selection.rangeCount !== 0) {
+			// Store the original range
+			const range = window.getSelection().getRangeAt(0);
+			console.log(range);
+			// Clone the range
+			const preCaretRange = range.cloneRange();
+			// Select all textual contents from the contenteditable element
+			preCaretRange.selectNodeContents(element);
+			// And set the range end to the original clicked position
+			preCaretRange.setEnd(range.endContainer, range.endOffset);
+			// Return the text length from contenteditable start to the range end
+			position = preCaretRange.toString().length;
+		}
+	}
+	return position;
 }
